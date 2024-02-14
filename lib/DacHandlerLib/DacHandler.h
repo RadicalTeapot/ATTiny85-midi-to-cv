b1e54e1bd5d7fd19bd276624c50e16bdcdc9d643
Mathias Capdet <mathias.capdet@tescan.com>
2024-02-14 22:52:12 +0100

    Cleanup

--- lib/DacHandlerLib/DacHandler.h
+++ lib/DacHandlerLib/DacHandler.h
@@ -1,24 +1,25 @@
 #ifndef DacHandler_h
 #define DacHandler_h
 
-#include <MidiEvent.h>
+#include "MidiEvent.h"
 #include "DacValues.h"
 #include "eventHandlers/DacEventHandler.h"
 
+class DacHandler
+{
+public:
+    typedef void (*WriteValuesToDac)(DacValues *dacValues);
+    static WriteValuesToDac writeValuesToDac;
 
-class DacHandler {
-    public:
-        typedef void (*WriteValuesToDac)(DacValues *dacValues);
-        static WriteValuesToDac writeValuesToDac;
+    ~DacHandler();
+    void setHandler(DacEventHandler *handler);
+    bool handleEvent(const MidiEvent *event);
 
-        ~DacHandler();
-        void setHandler(DacEventHandler *handler);
-        bool handleEvent(const MidiEvent *event);
-    private:
-        DacEventHandler *_handler = NULL;
-        DacValues _dacValues;
+private:
+    DacEventHandler *_handler = NULL;
+    DacValues _dacValues;
 
-        static void defaultWriteValuesToDac(DacValues *dacValues) {}
+    static void defaultWriteValuesToDac(DacValues *dacValues) {}
 };
 
 #endif // DacHandler_h
