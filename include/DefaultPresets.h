991f90f5e1170ac176898a65cd5b5f56ba403fb8
Mathias Capdet <35329469+RadicalTeapot@users.noreply.github.com>
2024-02-19 21:41:16 +0100

    Fix CC numbers

--- include/DefaultPresets.h
+++ include/DefaultPresets.h
@@ -6,19 +6,19 @@
 const Preset preset0 = {
     {
         0x01, // Note channels (1 and 2)
-        0x00, // CC number 1 (CC1)
-        0x01, // CC number 2 (CC2)
-        0x02, // CC number 3 (CC3)
-        0x03, // CC number 4 (CC4)
+        0x01, // CC number 1 (CC1)
+        0x02, // CC number 2 (CC2)
+        0x03, // CC number 3 (CC3)
+        0x04, // CC number 4 (CC4)
         0x00, // CC channels 1 (0)
         0x00, // CC channels 2 (0)
     },
     {
         0x23, // Note channels (3 and 4)
-        0x04, // CC number 1 (CC5)
-        0x05, // CC number 2 (CC6)
-        0x06, // CC number 3 (CC7)
-        0x07, // CC number 4 (CC8)
+        0x05, // CC number 1 (CC5)
+        0x06, // CC number 2 (CC6)
+        0x07, // CC number 3 (CC7)
+        0x08, // CC number 4 (CC8)
         0x00, // CC channels 1 (0)
         0x00, // CC channels 2 (0)
     }
@@ -27,19 +27,19 @@ const Preset preset0 = {
 const Preset preset1 = {
     {
         0x01, // Note channels (1 and 2)
-        0x00, // CC number 1 (CC1)
-        0x06, // CC number 2 (CC7)
-        0x0A, // CC number 3 (CC11)
-        0x49, // CC number 4 (CC74)
+        0x01, // CC number 1 (CC1)
+        0x07, // CC number 2 (CC7)
+        0x0B, // CC number 3 (CC11)
+        0x4A, // CC number 4 (CC74)
         0x00, // CC channels 1 (0)
         0x00, // CC channels 2 (0)
     },
     {
         0x23, // Note channels (3 and 4)
-        0x14, // CC number 1 (CC21)
-        0x15, // CC number 2 (CC22)
-        0x16, // CC number 3 (CC23)
-        0x46, // CC number 4 (CC71)
+        0x15, // CC number 1 (CC21)
+        0x16, // CC number 2 (CC22)
+        0x17, // CC number 3 (CC23)
+        0x47, // CC number 4 (CC71)
         0x00, // CC channels 1 (0)
         0x00, // CC channels 2 (0)
     }
