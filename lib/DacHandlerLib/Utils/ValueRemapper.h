bd107d7862951f0aa9dccf810f007aac2b9519d7
Mathias Capdet <mathias.capdet@tescan.com>
2024-02-28 10:44:59 +0100

    Constrain input in value remapper and add tests

--- lib/DacHandlerLib/Utils/ValueRemapper.h
+++ lib/DacHandlerLib/Utils/ValueRemapper.h
@@ -16,20 +16,28 @@ namespace ValueRemapper
 
     const uint8_t MIDI_MIN_VALUE = 0;
     const uint8_t MIDI_MAX_VALUE = 127;
+    const uint16_t DAC_MIN_VALUE = 0;
+    const uint16_t DAC_MAX_VALUE = 4095;
 
     template <uint8_t lowerMidiNote = DacPitchCalibration::LOW_MIDI_NOTE, uint8_t range = DacPitchCalibration::RANGE>
     inline uint16_t remapNote(const uint8_t midiNote)
     {
 #ifdef ARDUINO
-        return pgm_read_word_near(DacPitchCalibration::LookUpTable + min(midiNote - lowerMidiNote, range - 1));
+        return pgm_read_word_near(DacPitchCalibration::LookUpTable + constrain(midiNote - lowerMidiNote, 0, range - 1));
 #else
-        return DacPitchCalibration::LookUpTable[std::min(midiNote - lowerMidiNote, range - 1)];
+        return DacPitchCalibration::LookUpTable[std::min(std::max(midiNote - lowerMidiNote, 0), range - 1)];
 #endif
     }
-    template <uint8_t midiMinValue = MIDI_MIN_VALUE, uint8_t midiMaxValue = MIDI_MAX_VALUE>
+    template <uint8_t inputMinValue = MIDI_MIN_VALUE, uint8_t inputMaxValue = MIDI_MAX_VALUE, uint16_t outputMinValue = DAC_MIN_VALUE, uint16_t outputMaxValue = DAC_MAX_VALUE>
     inline uint16_t remapMidiValue(const uint8_t midiNote)
     {
-        return static_cast<uint16_t>(midiMinValue + midiMaxValue * (static_cast<float>(midiNote - midiMinValue) / (midiMaxValue - midiMinValue)));
+#ifdef ARDUINO
+        const uint8_t clampedMidiNote = constrain(midiNote, inputMinValue, inputMaxValue);
+#else
+        const uint8_t clampedMidiNote = std::max(inputMinValue, std::min(midiNote, inputMaxValue));
+#endif
+        const float mappedValue = static_cast<float>(clampedMidiNote - inputMinValue) / (inputMaxValue - inputMinValue);
+        return static_cast<uint16_t>(outputMinValue + (outputMaxValue - outputMinValue) * mappedValue);
     }
 }
 
---
+++ test/DacHandlerLib/test_ValueRemapper/test_ValueRemapper.cpp
@@ -0,0 +1,91 @@
+#include <unity.h>
+#include "Utils/ValueRemapper.h"
+
+void setUp() {}
+
+void tearDown() {}
+
+void test_ValueRemapper_midiValue_outOfRange_clamped()
+{
+    uint8_t input = 0;
+    uint16_t output = ValueRemapper::remapMidiValue<1, 2, 1, 2>(input);
+    TEST_ASSERT_EQUAL(1, output);
+
+    input = 3;
+    output = ValueRemapper::remapMidiValue<1, 2, 1, 2>(input);
+    TEST_ASSERT_EQUAL(2, output);
+}
+
+void test_ValueRemapper_midiValue_inRange_mapped()
+{
+    uint8_t input = 1;
+    uint16_t output = ValueRemapper::remapMidiValue<0, 10, 0, 10>(input);
+    TEST_ASSERT_EQUAL(1, output);
+
+    input = 1;
+    output = ValueRemapper::remapMidiValue<0, 1, 0, 10>(input);
+    TEST_ASSERT_EQUAL(10, output);
+
+    input = 1;
+    output = ValueRemapper::remapMidiValue<0, 2, 0, 10>(input);
+    TEST_ASSERT_EQUAL(5, output);
+}
+
+void test_ValueRemapper_midiValue_defaultValues()
+{
+    uint8_t input = 0;
+    uint16_t output = ValueRemapper::remapMidiValue(input);
+    TEST_ASSERT_EQUAL(0, output);
+
+    input = ValueRemapper::MIDI_MAX_VALUE;
+    output = ValueRemapper::remapMidiValue(input);
+    TEST_ASSERT_EQUAL(ValueRemapper::DAC_MAX_VALUE, output);
+}
+
+void test_ValueRemapper_noteValue_outOfRange_clamped()
+{
+    uint8_t input = DacPitchCalibration::RANGE + 1;
+    uint16_t output = ValueRemapper::remapNote<0, DacPitchCalibration::RANGE>(input);
+    TEST_ASSERT_EQUAL(DacPitchCalibration::LookUpTable[DacPitchCalibration::RANGE - 1], output);
+
+    input = 0;
+    output = ValueRemapper::remapNote<10, DacPitchCalibration::RANGE>(input);
+    TEST_ASSERT_EQUAL(DacPitchCalibration::LookUpTable[0], output);
+}
+
+void test_ValueRemapper_noteValue_inRange_mapped()
+{
+    uint8_t input = 1;
+    uint16_t output = ValueRemapper::remapNote<0, 10>(input);
+    TEST_ASSERT_EQUAL(DacPitchCalibration::LookUpTable[1], output);
+
+    input = 9;
+    output = ValueRemapper::remapNote<0, 10>(input);
+    TEST_ASSERT_EQUAL(DacPitchCalibration::LookUpTable[9], output);
+
+    input = 1;
+    output = ValueRemapper::remapNote<1, 10>(input);
+    TEST_ASSERT_EQUAL(DacPitchCalibration::LookUpTable[0], output);
+}
+
+void test_ValueRemapper_noteValue_defaultValues()
+{
+    uint8_t input = DacPitchCalibration::LOW_MIDI_NOTE;
+    uint16_t output = ValueRemapper::remapNote(input);
+    TEST_ASSERT_EQUAL(DacPitchCalibration::LookUpTable[0], output);
+
+    input = DacPitchCalibration::LOW_MIDI_NOTE + DacPitchCalibration::RANGE - 1;
+    output = ValueRemapper::remapNote(input);
+    TEST_ASSERT_EQUAL(DacPitchCalibration::LookUpTable[DacPitchCalibration::RANGE - 1], output);
+}
+
+int main() {
+    UNITY_BEGIN();
+    RUN_TEST(test_ValueRemapper_midiValue_outOfRange_clamped);
+    RUN_TEST(test_ValueRemapper_midiValue_inRange_mapped);
+    RUN_TEST(test_ValueRemapper_midiValue_defaultValues);
+    RUN_TEST(test_ValueRemapper_noteValue_outOfRange_clamped);
+    RUN_TEST(test_ValueRemapper_noteValue_inRange_mapped);
+    RUN_TEST(test_ValueRemapper_noteValue_defaultValues);
+    UNITY_END();
+}
